1032aa34871b7c4cb76623e3bd445a4246a4dd0d
Almog <almog.tsarfati@gmail.com>
2026-04-22 17:06:13 +0300

    add ci 5

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello from eks shimi1');
+  res.send('hello from eks shimi 5');
 });
 
 app.listen(port, () => {
